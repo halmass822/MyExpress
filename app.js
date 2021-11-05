17296befcf10ebfee36babaf0d644a1fde2beaef
Hazem A <halmassri822@gmail.com>
2021-11-05 17:37:54 -0400

    Changed data array to employee object array

--- app.js
+++ app.js
@@ -1,34 +1,32 @@
+const e = require('express');
 const express = require('express');
 const app = express();
 
-const dataArray = 'Hello World!'.split('');
+const Employees = [
+    {'employeeId': '1', 'name': 'John Doe', 'full time': 'false', 'join date': '2011/JAN/04'},
+    {'employeeId': '2', 'name': 'Jane Doe', 'full time': 'false', 'join date': '2015/MAR/18'},
+    {'employeeId': '3', 'name': 'Robert Bobbington', 'full time': 'true', 'join date': '2010/FEB/6'},
+    {'employeeId': '4', 'name': 'Harry Horton', 'full time': 'false', 'join date': '2017/DEC/19'},
+    {'employeeId': '5', 'name': 'Jim Jordan', 'full time': 'true', 'join date': '2010/SEP/21'}
+];
 
-app.get('/dataArray', (req, res, next) => {
-    res.status(200).send(dataArray);
+app.get('/Employees', (req, res, next) => {
+    res.status(200).send(Employees);
 });
 
-app.get('/dataArray/:index', (req, res, next) => {
-    const indexRequested = req.params.index;
-    //console.log(`Requested index is ${indexRequested}`);
-    const result = dataArray[indexRequested];
+app.get('/Employees/:employeeId', (req, res, next) => {
+    const employeeIdRequested = String(req.params.employeeId);
+    const result = Employees.find(({employeeId}) => {
+        return employeeId === employeeIdRequested;
+    })
     if(result) {
-        res.status(200).send(dataArray[indexRequested]);
+        res.status(200).send(result);
     } else {
         res.status(404).send();
+        console.log('Employee ID not found!');
     }
 });
-
-app.delete('/dataArray/:index', (req, res, next) => {
-    const indexRequested = req.params.index;
-    const result = dataArray[indexRequested]
-    if(result) {
-        dataArray.splice(indexRequested, 1);
-        res.status(204).send();
-    } else {
-        res.status(404).send();
-    }
-})
 
 app.listen(4001, () => {
-    console.log('Server listening on 4001\n version 1.1');
+    console.log('Server listening on 4001\n version 1.2');
 });
